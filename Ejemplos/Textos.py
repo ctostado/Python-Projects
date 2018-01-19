1f67224966a69027f2fefcf8f8422ca444781bd7
ctostado <cdanieltc@hotmail.com>
2018-01-19 10:24:21 -0700

    New Files

---
+++ Ejemplos/Textos.py
@@ -0,0 +1,32 @@
+c = "\t\t Ejercios para el uso de textos en Python"
+
+a = "Este es un ejemplo " 
+b = "de concatenacion de cadenas"
+
+e = "De esta manera podemos colocar 'Comillas dentro del codigo' "
+
+palabra = "Python"
+
+cuatroEspacios = " " * 4
+
+
+print (c)
+print ("\n")
+print ("\t" + a + b + "\n")
+print ("\t" + e + "\n")
+
+print (cuatroEspacios + "Multiplicacion de cadenas con espacios" + e + "\n")
+
+print ("\n")
+print ("\t\t Ejemplos Slicing en las cadenas")
+
+print ("\t" + palabra[0:2])
+print ("\t" + palabra[2:])
+print ("\t" + palabra[:])
+
+print ("\n")
+print ("\n")
+
+
+
+
---
+++ Ejemplos/teclado.py
@@ -0,0 +1,26 @@
+
+n = 0
+while n < 10:  # while = mientras
+    if (n % 2) == 0: # if = si (de condicion)
+        print(n,'es un número par')
+    else:  # else = sino (def condicion)
+        print(n,'es un número impar')
+    n = n + 1
+
+
+valor = input("Introduce un valor entero: ")
+
+valor = int(valor)
+
+entero = valor*10
+
+
+
+print(entero)
+
+
+
+
+
+
+
